221d633fdbb8896c8330f1ccae3293728de0f491
Murilo Marinho <murilo.marinho@manchester.ac.uk>
2026-03-05 15:01:22 +0000

    [docker] Compiling and testing wrapper

--- scripts/sas_robot_kinematics_interface_example.py
+++ scripts/sas_robot_kinematics_interface_example.py
@@ -1,8 +1,28 @@
-# Copyright (c) 2020-2022
-# Murilo Marques Marinho at the University of Tokyo.
-# This software can be used for Research Purposes only.
-# For commercial purposes, contact the author.
-# Author: Murilo M. Marinho, email: murilomarinho@ieee.org
+#!/usr/bin/python3
+"""
+# Copyright (c) 2020-2026 Murilo Marques Marinho
+#
+#    This file is part of sas_robot_kinematics.
+#
+#    sas_robot_kinematics is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    sas_robot_kinematics is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public License
+#    along with sas_robot_kinematics.  If not, see <https://www.gnu.org/licenses/>.
+#
+# #######################################################################################
+#
+#   Author: Murilo M. Marinho, email: murilomarinho@ieee.org
+#
+# #######################################################################################
+"""
 import time
 from dqrobotics import *  # Despite what PyCharm might say, this is very much necessary or DQs will not be recognized
 
